d2d9de6c4f7c47b0c79368d32d3dbddfbd362c31
mdAshikPradhan <mdashikpradhan.com>
2021-02-02 20:41:22 +0600

    Array, splice, join, slice

---
+++ array-split.js
@@ -0,0 +1,16 @@
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// // const part = nums.slice(2, 5);
+
+// const removed = nums.splice(2,3, 77, 88, 99);
+// // console.log(removed);
+// // console.log(nums);
+
+// const together = nums.join(" ");
+// console.log(together);
+
+const bismillah = ['In', 'the', 'name', 'of', 'Allah'];
+const part = bismillah.slice(2,5, 'Subahan', 'of', 'Allah');
+console.log(part);
+
+const together = bismillah.join(' ');
+console.log(together);
